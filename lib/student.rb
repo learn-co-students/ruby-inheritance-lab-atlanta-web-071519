5165b2a90b2e8a2437421943709972a356dc642b
John Odom <john.osborne.odom@gmail.com>
2019-07-20 18:16:14 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,25 @@
-class Student 
+class Student < User
+    attr_accessor :first_name, :last_name
+    def initialize
+        @first_name = first_name
+        @last_name = last_name
+        @knowledge = []
+    end
+
+    def first_name
+        @first_name
+    end
+
+    def last_name
+        @last_name
+    end
+
+    def learn (knowledge_string)
+        @knowledge << knowledge_string
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
